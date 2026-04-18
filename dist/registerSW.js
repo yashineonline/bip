1ffb5928e0a6dd21ab462911b1285d387d417b05
yashineonline <yashineonline@gmail.com>
2026-04-18 19:10:54 +0000

    Fix dependencies, typings, and GitHub Pages routing

--- dist/registerSW.js
+++ dist/registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('./sw.js', { scope: './' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/bip/sw.js', { scope: '/bip/' })})}
--- src/router.ts
+++ src/router.ts
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 
 const routes = [
   { path: '/', name: 'home', component: () => import('./pages/HomePage.vue'), meta: { title: 'Home' } },
@@ -24,7 +24,7 @@ const routes = [
 ]
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHashHistory(import.meta.env.BASE_URL),
   routes,
   scrollBehavior(to, from, savedPosition) {
     if (savedPosition) return savedPosition
